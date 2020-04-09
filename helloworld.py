0142f76e7bc6d55db07a98c0cbda7491f63f69a9
mapodangwona <mapo.dangwon.a@gmail.com>
2020-04-09 20:16:21 +0900

    First try to fulfillment

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,20 @@
+import json
 import os
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, world!'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    request = request.get_json(force=True)
+    
+    with open('data.json', 'r') as ifile:
+        data = json.load(ifile)
+
+    result = data['releaseDate'] + json.dumps(request)
+
+    return {'fulfillmentText': result}
