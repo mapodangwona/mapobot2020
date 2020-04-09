32ccf60891b378ad95e2df73763acc5fc4476c6b
mapodangwona <mapo.dangwon.a@gmail.com>
2020-04-09 21:00:14 +0900

    Next step

--- helloworld.py
+++ helloworld.py
@@ -8,13 +8,27 @@ app = Flask(__name__)
 def hello():
     return 'Hello, world!'
 
+DATA = None
+def _load_data():
+    global DATA
+    if not DATA:
+        with open('data.json', 'r') as ifile:
+            DATA = json.load(ifile)
+    return DATA
+
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     params = request.get_json(force=True)
-    
-    with open('data.json', 'r') as ifile:
-        data = json.load(ifile)
 
-    result = {'got': json.dumps(params), 'release': data['releaseDate']}
-
-    return {'fulfillmentText': json.dumps(result)}
+    intent = params['queryResult']['intent']['displayName']
+    if intent == 'candidate':
+        candidatename = params['queryResult']['parameters']['candidatename']
+        data = _load_data()['candidate'][candidatename]
+        return {'fulfillment_messages': [
+            {'text': data['text']},
+            {'image': data['image']}
+            {'link_out_suggestion': {'destination_name': '더보기', 'uri': data['url'}}
+        ]}
+    else:
+        return {'fulfillmentText': '챗봇이 이해할 수 없는 내용이어요 ㅠㅠ'}
