fdc27ed0d3bbde734b98dc4d61f81c94e95223d0
mapodangwona <mapo.dangwon.a@gmail.com>
2020-04-15 23:20:43 +0900

    HTML

--- helloworld.py
+++ helloworld.py
@@ -68,7 +68,7 @@ def webhook():
         result = {'fulfillment_messages': [], 'fulfillmentText': ""}
         is_google = False
         if intent == 'current':
-            result['fulfillment_messages'].append({'payload': {'telegram': {'text': '\n'.join(nec.stat()[0])}}})
+            result['fulfillment_messages'].append({'payload': {'telegram': {'text': '<br />\n'.join(nec.stat()[0], 'parse_mode': 'HTML')}}})
             return result
         if 'originalDetectIntentRequest' in params:
             if 'source' not in params['originalDetectIntentRequest']:
